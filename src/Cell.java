d0c5e14cc13c2641c268d1ec2449f5d56f775b19
Benny O <opw0011@gmail.com>
2016-09-23 11:13:43 +0800

    Add member varibles to Player

---
+++ src/Cell.java
@@ -0,0 +1,11 @@
+
+public class Cell {
+	private int x;
+	private int y;
+	private boolean canPass;
+	
+	public Cell() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
--- src/Player.java
+++ src/Player.java
@@ -1,8 +1,44 @@
+import java.util.ArrayList;
 
 public class Player {
-
-	public Player() {
-		// TODO Auto-generated constructor stub
+	private int x;
+	private int y;
+	private int numPokeBalls;
+	private ArrayList<Pokemon> pkmCaught;
+	private ArrayList<Cell> pathVisited;
+	
+	public Player(int x, int y) {
+		this.x = x;
+		this.y = y;
+		numPokeBalls = 0;
+		pkmCaught = new ArrayList<Pokemon>();
+		pathVisited = new ArrayList<Cell>();				
 	}
+
+	public int getX() {
+		return x;
+	}
+
+	public void setX(int x) {
+		this.x = x;
+	}
+
+	public int getY() {
+		return y;
+	}
+
+	public void setY(int y) {
+		this.y = y;
+	}
+
+	public int getNumPokeBalls() {
+		return numPokeBalls;
+	}
+
+	public void setNumPokeBalls(int numPokeBalls) {
+		this.numPokeBalls = numPokeBalls;
+	}
+	
+	
 
 }
